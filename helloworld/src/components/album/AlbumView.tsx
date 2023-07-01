592fc8bb55380945ad41274db4f4e8c951f3e85a
Maxim Janz <theropoy@fedora.fritz.box>
2023-07-01 17:47:13 +0200

    feat: Add username to user page and title to album page

--- helloworld/src/components/album/AlbumView.tsx
+++ helloworld/src/components/album/AlbumView.tsx
@@ -15,20 +15,32 @@ const fetchPhotos = (albumId: number) => {
     .then(response => response.json());
 };
 
+const fetchAlbum = (userId: number) => {
+    return fetch(`https://jsonplaceholder.typicode.com/albums/${userId}`)
+    .then(response => response.json());
+}
+
 const AlbumView = ({}) => {
     const {id} = useParams();
 
     const [photos, setPhotos] = useState<Photo[]>([]);
+    const [albumTitle, setAlbumTitle] = useState('');
 
     useEffect(() => {
         fetchPhotos(Number(id))
         .then((json) => {
             setPhotos(json);
         });
+
+        fetchAlbum(Number(id))
+        .then((json) => {
+            setAlbumTitle(json.title)
+        })
     }, [])
 
     return (
         <main>
+            <h1>{albumTitle}</h1>
             <PhotoList photos={photos}/>
         </main>
     )
--- helloworld/src/components/user/UserView.tsx
+++ helloworld/src/components/user/UserView.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import AlbumList from "./AlbumList";
+import { json } from "body-parser";
 
 export type Album = {
     userId: number,
@@ -13,20 +14,32 @@ const fetchAlbums = (userId: number) => {
     .then(response => response.json());
 };
 
+const fetchUser = (userId: number) => {
+    return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    .then(response => response.json());
+}
+
 const UserView = ({}) => {
     const {id} = useParams();
 
     const [albums, setAlbums] = useState<Album[]>([]);
+    const [user, setUser] = useState({name: '', username: ''});
 
     useEffect(() => {
         fetchAlbums(Number(id))
         .then((json) => {
             setAlbums(json);
         });
+
+        fetchUser(Number(id))
+        .then((json) => {
+            setUser({name: json.name, username: json.username})
+        })
     }, [])
 
     return (
         <main>
+            <h1>{user.name + ' - ' + user.username}</h1>
             <AlbumList albums={albums} />
         </main>
     )
