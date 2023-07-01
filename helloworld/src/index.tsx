ef13d77e0179bdff5258fc01829301ce8acd3dad
Maxim Janz <theropoy@fedora.fritz.box>
2023-07-01 14:57:08 +0200

    feat: Set up react router dom
    Installation and set up off react router dom. It will be used for the navigation between users and albums

--- helloworld/src/index.tsx
+++ helloworld/src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<App />}/>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
