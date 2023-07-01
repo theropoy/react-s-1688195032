0683cc7aad0135c7c3e3364b4c795a6f579c9298
Maxim Janz <theropoy@fedora.fritz.box>
2023-07-01 12:43:08 +0200

    test: Complete UserList test
    This test tests if the lists renders all users. It's gonna need to be rewritten if list switches to lazy loading

--- helloworld/src/styles/home/UserList.test.tsx
+++ helloworld/src/styles/home/UserList.test.tsx
@@ -1,14 +1,20 @@
+import { render, waitFor } from "@testing-library/react";
 import UserList from "../../components/home/UserList";
+import { User } from "../../utils/types";
 
 test('Test if all users in list rendered', async () => {
-    const {container} = render(<UserList users={}/>);
-  
-    waitFor(async () => {
-      const userList = container.getElementsByClassName('userlist')[0];
-      const testData = await fetch('https://jsonplaceholder.typicode.com/users').then((json) => json.length);
-      const shouldBeLength = testData
-      console.log(userList)
-      expect(userList.children.length).toEqual(0);
+    const testData = await fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json());
+
+    const userList: User[] = testData.map((user: any) => ({id: user.id, name: user.name, username: user.username, address: user.address}));
+    const shouldBeLength = userList.length;
+
+
+    const {container} = render(<UserList users={userList} />);
+
+    let childrenAmount = 0
+    await waitFor(() => {
+        childrenAmount = container.children[0].children.length;
     })
     
+    expect(childrenAmount).toEqual(shouldBeLength);
   });
