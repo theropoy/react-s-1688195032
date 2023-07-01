3ddb30f5bbc239b870bf0c207b47180517b64625
Maxim Janz <theropoy@fedora.fritz.box>
2023-07-01 09:37:26 +0200

    feat: Apply fetched user data into use state
    Prepares the data to pass into a user list later on

--- helloworld/src/App.tsx
+++ helloworld/src/App.tsx
@@ -1,14 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type User = {
+  id: number,
+  name: string,
+  username: string,
+  address: {street: string, suite: string, city: string, zipcode: string}
+}
+
 function App() {
 
+  const [users, setUsers] = useState<User[]>([]);
 
+  //fetch users on load
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(json => console.log(json))
+      .then(json => {
+        const userList: User[] = json.map((user: any) => ({id: user.id, name: user.id, username: user.username, address: user.address}));
+        setUsers(userList);
+      })
   }, [])
 
 
